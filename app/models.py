70f5ea56c819697d10f4f8182e3a9403ad3f3b81
pmhuizinga <pmhuizinga@hotmail.com>
2021-01-18 21:37:37 +0100

    model adjusted to GraphTool setup

--- app/models.py
+++ app/models.py
@@ -1,25 +1,46 @@
 from app import db
 
-class entity(db.Model):
+class node_type(db.Model):
 
-    __tablename__ = 'entities'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), index=True, unique=False)
-    lastname = db.Column(db.String(50), index=True, unique=False)
-    place_id = db.Column(db.Integer, db.ForeignKey('places.id'))
-
-    def __repr__(self):
-        return '{}, {}, {}'.format(self.name, self.lastname, self.place_id)
-
-
-class place(db.Model):
-
-    __tablename__ = 'places'
+    __tablename__ = 'node_type'
 
     id = db.Column(db.Integer, primary_key=True)
-    place = db.Column(db.String(30), index=True, unique=True)
-    entity = db.relationship('entity', backref='place', lazy='dynamic')
+    name = db.Column(db.String(40), index=True, unique=False)
+    node = db.relationship('node', backref='place', lazy='dynamic')
 
     def __repr__(self):
-        return '{}'.format(self.place)
+        return '{}'.format(self.name)
+
+class node(db.Model):
+
+    __tablename__ = 'node'
+
+    id = db.Column(db.Integer, primary_key=True)
+    node_type_id = db.Column(db.Integer, db.ForeignKey('node_type.id'))
+    name = db.Column(db.String(40), index=True, unique=False)
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+class relation_type(db.Model):
+
+    __tablename__ = 'relation_type'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), index=True, unique=False)
+    relation = db.relationship('node', backref='place', lazy='dynamic')
+
+    def __repr__(self):
+        return '{}'.format(self.name)
+
+class relation(db.Model):
+
+    __tablename__ = 'relation'
+
+    id = db.Column(db.Integer, primary_key=True)
+    source_node_id = db.Column(db.Integer, db.ForeignKey('node.id'))
+    target_node_id = db.Column(db.Integer, db.ForeignKey('node.id'))
+    relation_type_id = db.Column(db.Integer, db.ForeignKey('relation_type.id'))
+
+    def __repr__(self):
+        return '{}'.format(self.id)
