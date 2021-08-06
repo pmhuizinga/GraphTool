31dd37ccbec9a6d1b75abcf20fcf733c7c9619b4
pmhuizinga <pmhuizinga@hotmail.com>
2021-08-06 12:35:42 +0200

    directory commit

--- app/functions/import_export.py
+++ app/functions/import_export.py
@@ -1,20 +1,43 @@
 import json
 from py2neo import Graph, Node, NodeMatcher, Relationship
+import requests
+
 graph = Graph(host="localhost", port=7687, auth=('neo4j', 'admin'))
 matcher = NodeMatcher(graph)
 
 # todo: make new api for all nodes/edges including properties
 
-f = 'C:\\Users\\pahuizinga\\OneDrive - Aegon\\Python\\GraphTool\\documentation\\'
+f = 'C:\\Users\\PaulMarjanIlseMeike\\Dropbox\\Paul\\DataScience\\Projects\\GraphToolNeo4j\\documentation\\'
 nodes_url = 'http://127.0.0.1:5000/graph_nodes/node/all'
-edges_url =  'http://127.0.0.1:5000/graph_edges/node/all'
+edges_url = 'http://127.0.0.1:5000/graph_edges/node/all'
 
 
-def read_from_api():
-    pass
+def clear_database():
+    delete_query = "MATCH(n) DETACH DELETE n"
+    graph.run(delete_query)
 
-def read_from_file(folderpath, nodes_file_name = 'nodes.json', edges_file_name='edges.json'):
 
+def read_from_api(url):
+    """
+    read api data and return as json file
+    """
+    r = requests.get(url)
+    data = r.json()
+    return data
+
+
+def write_to_files(folderpath, nodes_file_name='nodes.json', edges_file_name='edges.json'):
+    """
+    Write node file and edge file from api to a specified location
+    """
+    with open(folderpath + nodes_file_name, 'w') as f:
+        json.dump(read_from_api(nodes_url), f)
+
+    with open(folderpath + edges_file_name, 'w') as f:
+        json.dump(read_from_api(edges_url), f)
+
+
+def read_from_file(folderpath, nodes_file_name='nodes.json', edges_file_name='edges.json'):
     # read files
     # with open('C:\\Users\\pahuizinga\\OneDrive - Aegon\\Python\\GraphTool\\documentation\\nodes.json', 'r') as myfile:
     with open(folderpath + nodes_file_name, 'r') as myfile:
@@ -24,22 +47,18 @@ def read_from_file(folderpath, nodes_file_name = 'nodes.json', edges_file_name='
 
     # with open('C:\\Users\\pahuizinga\\OneDrive - Aegon\\Python\\GraphTool\\documentation\\edges.json', 'r') as myfile:
     with open(folderpath + edges_file_name, 'r') as myfile:
-        edge_set=myfile.read()
+        edge_set = myfile.read()
     edges = json.loads(edge_set)
 
     return nodes, edges
 
-a, b = read_from_file(folderpath=f)
-
-def write_to_files(folderpath, nodes_file_name = 'nodes.json', edges_file_name='edges.json'):
-
-    pass
 
 def create_nodes_in_neo4j(nodes):
     # create nodes in Neo
     for item in nodes:
         a = Node(item['type'], name=item['id'])
         graph.create(a)
+
 
 def create_edges_in_neo4j(edges):
     # create edges in Neo
@@ -49,3 +68,8 @@ def create_edges_in_neo4j(edges):
         a = Relationship(NodeSource, item['type'], NodeTarget)
         graph.create(a)
 
+
+def create_database(folderpath):
+    nodes, edges = read_from_file(folderpath)
+    create_nodes_in_neo4j(nodes)
+    create_edges_in_neo4j(edges)
