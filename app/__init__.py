c97dcdca4d6f20edd398c3fa812ec46f93214242
pahuizinga <pmhuizinga@protonmail.com>
2021-08-09 18:55:00 +0200

    neo4j directory commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,23 @@
 # todo: add "ID already exists" warning
 # todo: merge epic and project to project, include type 'epic'
 # todo: put db conn in config
-# todo: create blank db as an option
-# todo: select db as an option
-# todo: backup database
 # todo: add function to merge tables
 # todo: add function to rename table name
-# todo: auto 'close' relations when a node is 'closed' (with dates).
 # todo: add possibility to remove or modify an edge
 # todo: add full dependencies as visual
 # todo: switch graph view to community view (and back)
 # todo: learn BERT for NLP
 # todo: add 'api/' to api string
 # todo: move all database actions to database_functions.py (not in views.py)
+# todo: make new api for all nodes/edges including properties
 
+# done: create blank db as an option
+# done: select db as an option
+# done: backup database
 # done: drop collection when empty
 # done: remove main.css / create.html
 # done: make remove key function an api
 # done: make nodes stick so it's easier to add multiple relations to the same node
-# rejected: remove duplicate edges (or do not allow to be stored)
-# rejected: rebase to objectID (not possible, you do not want to look for characteristics but for id's. It has to be unique)
 # done: add trim function when adding new collection (names)
 # done: add default 'geldigheid' to edges
 # done: add edge properties
@@ -34,6 +32,9 @@
 # done: add color code to nodes in graph
 # done: avoid adding duplicate edges
 # done: add view on a single node (view all nodes relations)
+# rejected: auto 'close' relations when a node is 'closed' (with dates). (no longer needed: neo4j does automatically)
+# rejected: remove duplicate edges (or do not allow to be stored)
+# rejected: rebase to objectID (not possible, you do not want to look for characteristics but for id's. It has to be unique)
 
 from flask import Flask
 from py2neo import Graph
--- app/functions/import_export.py
+++ app/functions/import_export.py
@@ -1,18 +1,22 @@
 import json
 from py2neo import Graph, Node, NodeMatcher, Relationship
 import requests
+import os
+# from app.home import home
+# from flask import url_for
 
 graph = Graph(host="localhost", port=7687, auth=('neo4j', 'admin'))
 matcher = NodeMatcher(graph)
 
-# todo: make new api for all nodes/edges including properties
-
-f = 'C:\\Users\\PaulMarjanIlseMeike\\Dropbox\\Paul\\DataScience\\Projects\\GraphToolNeo4j\\documentation\\'
+# f = 'C:\\Users\\pahuizinga\\OneDrive - Aegon\\Python\\GraphTool\\documentation\\'
+f = 'C:\\Users\\pahuizinga\\OneDrive - Aegon\\Python\\GraphTool\\databases\\'
 nodes_url = 'http://127.0.0.1:5000/graph_nodes/node/all'
+# nodes_url = url_for('home.get_graph_nodes', base='node', id='all')
 edges_url = 'http://127.0.0.1:5000/graph_edges/node/all'
+# edges_url = url_for('home.get_graph_edges', base='node', id='all')
 
 
-def clear_database():
+def database_clear():
     delete_query = "MATCH(n) DETACH DELETE n"
     graph.run(delete_query)
 
@@ -26,7 +30,7 @@ def read_from_api(url):
     return data
 
 
-def write_to_files(folderpath, nodes_file_name='nodes.json', edges_file_name='edges.json'):
+def database_save(folderpath, nodes_file_name='nodes.json', edges_file_name='edges.json'):
     """
     Write node file and edge file from api to a specified location
     """
@@ -69,7 +73,75 @@ def create_edges_in_neo4j(edges):
         graph.create(a)
 
 
-def create_database(folderpath):
+def database_open(folderpath, db_delete=True):
+    """
+    Create a database in Neo4j based on source node and edge files
+    """
+    if db_delete == True:
+        database_clear()
+
     nodes, edges = read_from_file(folderpath)
     create_nodes_in_neo4j(nodes)
     create_edges_in_neo4j(edges)
+
+def read_current_db_name():
+    with open(f + 'current_db.txt', 'r') as myfile:
+        db_name = myfile.read()
+
+    return db_name
+
+def write_db_name(db_name):
+    with open(f + 'current_db.txt', 'w') as myfile:
+        myfile.write(db_name)
+        print('current_db was changed to {}'.format(db_name))
+
+def database_create(new_db_name, save_current_db=True):
+    """
+    create a new database. Current database will be saved
+    """
+    # save current database
+    if save_current_db == True:
+
+        db = read_current_db_name(f)
+        save_folder = f + db + '\\'
+        database_save(save_folder)
+        print('Database {} was saved to {}'.format(db, save_folder))
+
+    # clear current database
+    database_clear()
+    print('Current Neo4j database is truncated')
+
+    # create new database
+    # create folder
+    newpath = f + new_db_name
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+        print('folder {} is created'.format(new_db_name))
+
+    # change current_db.txt
+    write_db_name(new_db_name)
+
+def database_switch(db_name, save_current_db=True):
+
+    # save current database
+    if save_current_db == True:
+
+        db = read_current_db_name()
+        save_folder = f + db + '\\'
+        database_save(save_folder)
+        print('Database {} was saved to {}'.format(db, save_folder))
+
+    # clear current database
+    database_clear()
+    print('Current Neo4j database is truncated')
+
+    # open new database
+    database_open(f + db_name + '\\')
+    print('Database {} is loaded'.format(db_name))
+
+    # change current_db.txt
+    write_db_name(db_name)
+
+
+
+
